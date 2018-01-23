84e50195dbf46487fefd13878d41a5f78174d7fa
chang0022 <chang.0022@163.com>
2018-01-24 00:40:06 +0800

    修改 使用 when 代替 回调

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,4 +1,4 @@
-import { action, useStrict } from 'mobx'
+import { observable, action, useStrict, runInAction } from 'mobx'
 import Axios from 'axios'
 import { Toast } from 'antd-mobile'
 import status from './status'
@@ -6,31 +6,21 @@ import status from './status'
 useStrict(true)
 
 class Auth {
-  //   @action.bound
-  //   async login(userInfo) {
-  //     try {
-  //       status.setLoading(true)
-  //       await Axios.post('/api/auth', userInfo)
-  //     } catch (err) {
-  //       Toast.fail('系统异常', 2)
-  //     } finally {
-  //       status.setLoading(false)
-  //     }
-  //   }
+  @observable isLogin = false
+
   @action.bound
-  asyncLogin(userInfo) {
-    status.setLoading(true)
-    return new Promise((resolve, reject) => {
-      Axios.post('/api/auth', userInfo)
-        .then(() => {
-          resolve()
-          status.setLoading(false)
-        })
-        .catch(err => {
-          Toast.fail('系统异常', 2)
-          status.setLoading(false)
-        })
-    })
+  async loginHandler(userInfo) {
+    try {
+      status.setLoading(true)
+      await Axios.post('/api/auth', userInfo)
+      runInAction(() => {
+        this.isLogin = true
+      })
+    } catch (err) {
+      Toast.fail('系统异常', 2)
+    } finally {
+      status.setLoading(false)
+    }
   }
 }
 
