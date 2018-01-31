829ac92b341f09955cbb4f03b8c9ee619083402a
chang0022 <chang.0022@163.com>
2018-02-01 01:52:36 +0800

    添加 简单的购物车原型

--- src/components/shop/Cart.js
+++ src/components/shop/Cart.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import classNames from 'classnames'
 import styles from './Cart.css'
 
 const Cart = props => {
   return (
     <div className={styles.cart}>
-      <div>111</div>
+      <div
+        className={classNames(styles.icon, {
+          [styles.full]: true
+        })}
+      >
+        <i className="iconfont icon-shop-order" />
+      </div>
+      <div className={styles.cartInfo}>
+        <span>133.1</span>
+      </div>
+      <div
+        className={classNames(styles.cartButton, {
+          [styles.ready]: true
+        })}
+      >
+        去结算
+      </div>
     </div>
   )
 }
