46870bc2f7d7aa242490ba3db7a295eb4d8d25d2
Neo <chang.0022@163.com>
2018-02-08 20:18:40 +0800

    添加 销售订单详情

---
+++ src/pages/store/OrderDetail.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Order from '../../components/order'
+
+class Detail extends Component {
+  render() {
+    return <Order />
+  }
+}
+
+export default Detail
